9c89450432b104d45411b95467d90c3d43009d49
Ryan Lewis <ryan.lewis@aptitive.com>
2020-05-16 13:00:43 -0500

    Translate mrx file from ruby to python

--- mrx.py
+++ mrx.py
@@ -1,41 +1,28 @@
-class MrXPlayer
+import random
+
+class MrXPlayer:
 
     # play gets called by the controller once per turn
-    def play(gamestate)
-        play_greedy(gamestate[:routes], gamestate[:positions_of_agents], gamestate[:board])
-
-    end
+    def play(self, gamestate):
+        return self.play_greedy(gamestate['routes'], gamestate['positions_of_agents'], gamestate['board'])
 
     # Play to a random station
-    def play_random(routes)
-        routes.sample
+    def play_random(self, routes):
+        return random.sample(routes,1)
 
-    end
-
-    # Play to the station with the maximal avarage distance to all agents
-    # using a greedy algorithm
-    def play_greedy(routes, positions_of_agents, board)
+    # Play to the station with the maximal average distance to all agents using a greedy algorithm
+    def play_greedy(self, routes, positions_of_agents, board):
         max_average_distance_to_agents = 0
         route_to_go_to = 0
-        routes.each_with_index
-        do | route, index |
-        my_position = route[:station]
-        sum_of_distances_to_agents = 0
-        positions_of_agents.each
-        do | position_of_agent |
-        current_distance_to_agent = board.distance(my_position, position_of_agent)
-        sum_of_distances_to_agents += current_distance_to_agent
+        for index, route in enumerate(routes):
+            my_position = route['station']
+            sum_of_distances_to_agents = 0
+            for position_of_agent in positions_of_agents:
+                current_distance_to_agent = board.distance(my_position, position_of_agent)
+                sum_of_distances_to_agents += current_distance_to_agent
+            current_average_distance_to_agents = sum_of_distances_to_agents / 4 # TODO: This 4 should be replaced by no. of agents variable
+            if current_average_distance_to_agents > max_average_distance_to_agents:
+                max_average_distance_to_agents = current_average_distance_to_agents
+                route_to_go_to = index
+        return routes[route_to_go_to]
 
-    end
-    current_avgerage_distance_to_agents = sum_of_distances_to_agents / 4
-    if current_avgerage_distance_to_agents > max_average_distance_to_agents
-        max_average_distance_to_agents = current_avgerage_distance_to_agents
-        route_to_go_to = index
-    end
-
-
-end
-routes[route_to_go_to]
-end
-
-end
