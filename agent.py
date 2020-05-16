6c5caf61f7a58ef88318e394a48355fb25ca249f
Ryan Lewis <ryan.lewis@aptitive.com>
2020-05-16 14:40:14 -0500

    Translate agent file from ruby to python

--- agent.py
+++ agent.py
@@ -1,58 +1,48 @@
+import random
 
-class AgentPlayer
-  
-  def initialize
-    @@possible_positions_mrx = []
-  end
-  
-  # play gets called by the controller once per turn
-  def play(gamestate)
-    if gamestate[:figure_id] == 1
-      calculate_possible_positions_mrx(gamestate[:position_of_mrx], gamestate[:mrx_last_used_ticket], gamestate[:board])
-    end
-    play_greedy(gamestate[:routes], gamestate[:board])
-  end
-  
-  # Play to a random station
-  def play_random(routes)
-    routes.sample
-  end
-  
-  def play_greedy(routes, board)
-    return nil if routes.empty?
-    return play_random(routes) if @@possible_positions_mrx.empty?
-    min_average_distance_to_mrx = 10
-    route_to_go_to = 0
-    routes.each_with_index do |route, index|
-      my_position = route[:station]
-      sum_of_distances_to_mrx = 0
-      @@possible_positions_mrx.each do |position_of_mrx|
-        current_distance_to_mrx = board.distance(my_position, position_of_mrx)
-        sum_of_distances_to_mrx += current_distance_to_mrx
-      end
-      current_avgerage_distance_to_mrx = sum_of_distances_to_mrx / @@possible_positions_mrx.length
-      if current_avgerage_distance_to_mrx < min_average_distance_to_mrx
-        min_average_distance_to_mrx = current_avgerage_distance_to_mrx
-        route_to_go_to = index
-      end
-    end
-    return routes[route_to_go_to]
-  end
-  
-  def calculate_possible_positions_mrx(position_of_mrx, mrx_last_used_ticket, board)
-    if position_of_mrx == 0
-      new_positions = []
-      @@possible_positions_mrx.each do |p|
-        board.routes_from(p).each do |q|
-          if q[:ticket] == mrx_last_used_ticket and not new_positions.include?(q[:station])
-            new_positions << q[:station]
-          end
-        end
-      end
-      @@possible_positions_mrx = new_positions.sort
-    else
-      @@possible_positions_mrx = [position_of_mrx]  
-    end
-  end
-  
-end
+
+class AgentPlayer:
+
+    def __init__(self):
+        self.possible_positions_mrx = []
+
+    # play gets called by the controller once per turn
+    def play(self, gamestate):
+        if gamestate['figure_id'] == 1:
+            self.calculate_possible_positions_mrx(gamestate['position_of_mrx'], gamestate['mrx_last_used_ticket'],
+                                                  gamestate['board'])
+        return self.play_greedy(gamestate['routes'], gamestate['board'])
+
+    # Play to a random station
+    def play_random(self, routes):
+        return random.sample(routes, 1)
+
+    def play_greedy(self, routes, board):
+        if not routes:
+            return None
+        if not self.possible_positions_mrx:
+            return self.play_random(routes)
+        min_average_distance_to_mrx = 10
+        route_to_go_to = 0
+        for index, route in enumerate(routes):
+            my_position = route['station']
+            sum_of_distances_to_mrx = 0
+            for position_of_mrx in self.possible_positions_mrx:
+                current_distance_to_mrx = board.distance(my_position, position_of_mrx)
+                sum_of_distances_to_mrx += current_distance_to_mrx
+            current_avgerage_distance_to_mrx = sum_of_distances_to_mrx / len(self.possible_positions_mrx)
+            if current_avgerage_distance_to_mrx < min_average_distance_to_mrx:
+                min_average_distance_to_mrx = current_avgerage_distance_to_mrx
+                route_to_go_to = index
+        return routes[route_to_go_to]
+
+    def calculate_possible_positions_mrx(self, position_of_mrx, mrx_last_used_ticket, board):
+        if position_of_mrx == 0:
+            new_positions = []
+            for p in self.possible_positions_mrx:
+                for q in board.routes_from(p):
+                    if q['ticket'] == mrx_last_used_ticket and not q['station'] in new_positions:
+                        new_positions.append(q['station'])
+            self.possible_positions_mrx = new_positions.sort()
+        else:
+            self.possible_positions_mrx = [position_of_mrx]
