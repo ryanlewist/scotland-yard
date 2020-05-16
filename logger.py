0fa06eb2743bf89ac964fbfe41d58dca8478e784
Ryan Lewis <ryan.lewis@aptitive.com>
2020-05-16 15:22:21 -0500

    Translate logger file from ruby to python

--- logger.py
+++ logger.py
@@ -1,18 +1,19 @@
+from datetime import datetime
 
-class Logger
-  
-  def initialize(filename = 'log.md')
-    @filename = filename
-    @contents = "*Date: #{Time.now}*\n"
-    @contents << "----------------------------------------\n"
-  end
-  
-  def log(message)
-    @contents << message << "\n"
-  end
-  
-  def save
-    File.open(@filename, 'w') {|f| f.puts(@contents)}
-  end
-  
-end
+
+class Logger:
+
+    def __init__(self, filename='log.md'):
+        self.filename = filename
+        self.contents = ["Date: {0}\n".format(datetime.now())]
+        self.contents.append("----------------------------------------\n")
+
+    def log(self, message):
+        self.contents.append(message)
+        self.contents.append('\n')
+
+    def save(self):
+        f = open(self.filename, "w")
+        f.write(''.join(self.contents))
+        f.close()
+
