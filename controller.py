dc03c2a1e9f6b6bab83a25cf16b77daafdd359a2
Ryan Lewis <ryan.lewis@aptitive.com>
2020-05-17 00:32:38 -0500

    Alias game import to the_game to avoid confusion/conflict

--- controller.py
+++ controller.py
@@ -1,4 +1,4 @@
-import game
+import game as the_game
 import mrx
 import agent
 
@@ -6,7 +6,7 @@ import agent
 class Controller:
 
     def run(self):
-        game = game.Game()
+        game = the_game.Game()
         players = [mrx.MrXPlayer(), agent.AgentPlayer(), agent.AgentPlayer(), agent.AgentPlayer(), agent.AgentPlayer()]
         while True:
             for i, p in enumerate(players):
