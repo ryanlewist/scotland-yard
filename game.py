7bc6d25120fbdf192a2129bec9a8d5cd3bb16127
Ryan Lewis <ryan.lewis@aptitive.com>
2020-05-16 20:54:46 -0500

    Translate game file from ruby to python

--- game.py
+++ game.py
@@ -1,120 +1,114 @@
+import random
+import board
+import figure
+import logger
 
-require 'board'
-require 'figure'
-require 'logger'
 
 # The class Game fully describes Scotland Yard:
 # The board, Mr. X and the agents.
 # There are higher-level methods that change the state of the game.
 
-class Game
-  
-  # Initialization creates a board and the figures
-  # Then the figures are put on random starting positions
-  def initialize
-    puts "Starting game"
-    puts "Loading board..."
-    @board = Board.new
-    start = [13, 26, 29, 34, 50, 53, 91, 94, 103, 112, 117, 132, 138, 141, 155, 174, 197, 198].shuffle
-    @figures = []
-    for i in 0..4
-      @figures << Figure.new(i, start.pop)
-    end
-    @turns = 0
-    @mrx_last_used_ticket = nil
-    @logger = Logger.new
-    log "## SCOTLAND YARD: MR. X VS. 4 AGENTS"
-  end
-  
-  def log(msg)
-    @logger.log(msg)
-  end
-  
-  # Is the game over?
-  # There are two possibilities:
-  # - Mr. X and an agent are at the same station (=> The agents win)
-  # - 20 turns have passed (=> Mr. X has won)
-  def over?
-    for i in 1..4
-      if @figures[0].position == @figures[i].position
-        log "**GAME OVER:** Mr. X was caught - The agents win."
-        @logger.save
-        puts "Game Over: The agents win"
-        return true
-      end
-    end
-    if @turns > 19
-      log "**GAME OVER:** 20 turns have passed - Mr. X wins."
-      @logger.save
-      puts "Game Over: Mr. X wins"
-      return true
-    end
-    return false
-  end
-  
-  # Returns the position of Mr. X at the 3., 8., 13., 18. and 23. turn or 0.
-  def position_of_mrx
-    if @turns % 5 == 3
-      @figures[0].position
-    else
-      0
-    end
-  end
-  
-  # Returns an array of the positions of the agents.
-  def positions_of_agents
-    positions = []
-    @figures.each {|f| positions << f.position}
-    positions.shift
-    positions
-  end
-  
-  def tickets
-    tickets = []
-    @figures.each {|f| tickets << f.tickets}
-    tickets
-  end
-  
-  def possible_routes_for(figure)
-    routes = @board.routes_from(figure.position)
-    routes.delete_if {|r| figure.tickets[r[:ticket]] == 0}
-    if figure.id > 0
-      routes.delete_if {|r| positions_of_agents.include?(r[:station])}
-    end
-    routes
-  end
-  
-  # Moves the fgure with figure_id to [station] by [ticket].
-  def move(figure_id, station, ticket)
-    figure = @figures[figure_id]
-    routes = possible_routes_for(figure)
-    if routes.include?({station: station, ticket: ticket}) or ticket == :black
-      if figure.id == 0
-        log "\n### TURN #{@turns + 1}:"
-        @turns += 1
-        @mrx_last_used_ticket = ticket
-      end
-      figure_name = figure.id == 0 ? "Mr. X" : "Agent #{figure.id}"
-      log "**#{figure_name}** moves from *#{figure.position}* to *#{station}* by *#{ticket}*\n"
-      figure.tickets[ticket] -= 1
-      figure.position = station
-      return true
-    end
-    return false
-  end
-  
-  # Describes the current gamstate
-  def gamestate(figure_id)
-    {
-      figure_id: figure_id,
-      routes: possible_routes_for(@figures[figure_id]),
-      board: @board,
-      position_of_mrx: position_of_mrx,
-      positions_of_agents: positions_of_agents,
-      turns: @turns,
-      tickets: tickets,
-      mrx_last_used_ticket: @mrx_last_used_ticket
-    }
-  end
-  
-end
+
+class Game:
+
+    # Initialization creates a board and the figures
+    # Then the figures are put on random starting positions
+    def __init__(self):
+        print("Starting game")
+        print("Loading board...")
+        self.board = board.Board()
+        all_starts = [13, 26, 29, 34, 50, 53, 91, 94, 103, 112, 117, 132, 138, 141, 155, 174, 197, 198]
+        start = random.sample(all_starts, 5)
+        self.figures = []
+        for i in range(0, 5):
+            self.figures.append(figure.Figure(i, start[i]))
+        self.turns = 0
+        self.mrx_last_used_ticket = None
+        self.logger = logger.Logger()
+        self.logger.log("## SCOTLAND YARD: MR. X VS. 4 AGENTS")
+
+    def log(self, msg):
+        self.logger.log(msg)
+
+    # Is the game over?
+    # There are two possibilities:
+    # - Mr. X and an agent are at the same station (=> The agents win)
+    # - 20 turns have passed (=> Mr. X has won)
+    def is_over(self):
+        for i in range(0, 5):
+            if self.figures[0].position == self.figures[i].position:
+                self.logger.log("**GAME OVER:** Mr. X was caught - The agents win.")
+                self.logger.save()
+                print("Game Over: The agents win")
+                return True
+        if self.turns > 19:
+            self.logger.log("**GAME OVER:** 20 turns have passed - Mr. X wins.")
+            self.logger.save()
+            print("Game Over: Mr. X wins")
+            return True
+        return False
+
+    # Returns the position of Mr. X at the 3, 8, 13, and 18 turns.
+    def position_of_mrx(self):
+        if self.turns % 5 == 3:
+            return self.figures[0].position()
+        else:
+            return 0
+
+    # Returns an array of the positions of the agents.
+    def positions_of_agents(self):
+        positions = []
+        for f in self.figures:
+            positions.append(f.position)
+        positions.pop(0)  # Remove Mr. X's position
+        return positions
+
+    def tickets(self):
+        tickets = []
+        for f in self.figures:
+            tickets.append(f.tickets)
+        return tickets
+
+    def possible_routes_for(self, figure):
+        routes = self.board.routes_from(figure.position)
+        routes[:] = [r for r in routes if figure.tickets[r['ticket']] != 0]
+        # TODO: Not sure how the following piece works. I think it means that you can't move
+        #  to the same space as  another agent, but I'm not sure why it only applies to the agents
+        #  and not Mr. X,  who also can't move into the same space as an agent.
+        if figure.id > 0:
+            routes[:] = [r for r in routes if self.positions_of_agents != r['station']]
+        return routes
+
+    # Moves the figure with figure_id to [station] by [ticket].
+    def move(self, figure_id, station, ticket):
+        figure = self.figures[figure_id]
+        routes = self.possible_routes_for(figure)
+        if {'station': station, 'ticket': ticket} in routes or ticket == 'black':
+            if figure.id == 0:
+                self.logger.log("\n### TURN {0}:").format(self.turns + 1)
+                self.turns += 1
+                self.mrx_last_used_ticket = ticket
+            if figure.id == 0:
+                figure_name = 'Mr. X'
+            else:
+                figure_name = 'Agent {0}'.format(figure.id)
+            self.logger.log("**{figure_name}** moves from *{figure_position}* to *{station}* by *{ticket}*\n").format(
+                figure_name=figure_name, figure_position=figure.position, station=station, ticket=ticket)
+            figure.tickets[ticket] -= 1
+            figure.position = station
+            return True
+        return False
+
+    # Describes the current gamestate
+    def gamestate(self, figure_id):
+        state = {
+            'figure_id': figure_id,
+            'routes': self.possible_routes_for(self.figures[figure_id]),
+            'board': self.board,
+            'position_of_mrx': self.position_of_mrx,
+            'positions_of_agents': self.positions_of_agents,
+            'turns': self.turns,
+            'tickets': self.tickets,
+            'mrx_last_used_ticket': self.mrx_last_used_ticket
+        }
+        return state
