e0d492c479b25a4f5ace9dc49ac4ad3736085147
Ryan Lewis <ryan.lewis@aptitive.com>
2020-05-23 21:40:12 -0500

    Translate board unit tests to python

--- tests/test_board.py
+++ tests/test_board.py
@@ -1,160 +1,35 @@
-require
-'test/unit'
-require
-'game'
-
-
-class TestGame < Test::
-
-
-    Unit::TestCase
-
-
-def setup
-    @game
-
-    = Game.new
-
-    @game.figures
-
-    [0].position = 56
-
-    @game.figures
-
-    [1].position = 84
-
-    @game.figures
-
-    [2].position = 37
-
-    @game.figures
-
-    [3].position = 23
-
-    @game.figures
-
-    [4].position = 111
-
-
-end
-
-
-def test_initial_setup
-    assert_equal
-    5,
-
-    @game.figures.length
-    @game.figures.each
-
-    do | figure |
-    assert [111, 56, 84, 37, 159, 23].include?(figure.position)
-
-
-end
-assert_equal
-0,
-
-
-@game.turns
-
-
-assert_equal[],
-
-
-@game.mrx_log
-
-
-assert_equal[],
-
-
-@game.agents_log
-
-
-end
-
-
-def test_moving
-    assert
-
-    @game.move(0, 91,
-
-    :taxi)
-    assert_equal
-    91,
-
-    @game.figures
-
-    [0].position
-    assert_equal
-    98,
-
-    @game.figures
-
-    [0].tickets[:taxi]
-    refute @ game.move(0, 1,: taxi)
-    assert_equal
-    1,
-
-    @game.turns
-
-    assert_equal[0],
-
-    @game.mrx_log
-
-
-end
-
-
-def test_moving_agent_on_agent
-    @game.figures
-
-    [1].position = 22
-
-    @game.figures
-
-    [2].position = 34
-    refute @ game.move(2, 22,: bus)
-    end
-
-    def test_game_over_with_turns
-        @game.turns
-
-        = 20
-        assert
-
-        @game.move(0, 42,
-
-        :taxi)
-        assert_equal
-        21,
-
-        @game.turns
-
-        assert
-
-        @game.over
-
-        ?
-        end
-
-        def test_game_over_because_caught
-            @game.figures
-
-            [0].position = 77
-
-            @game.figures
-
-            [1].position = 94
-            assert
-
-            @game.move(1, 77,
-
-            :bus)
-            assert
-
-            @game.over
-
-            ?
-            end
-
-        end
+import pytest
+import board
+import os
+
+# Change working directory to project folder in order to be able to import yml files
+os.chdir('..')
+
+@pytest.fixture
+def sy_board():
+    return board.Board()
+
+
+def test_routes_from_158(sy_board):
+    routes_from_158_list = [
+        {'station': 141, 'ticket': 'taxi'},
+        {'station': 142, 'ticket': 'taxi'},
+        {'station': 157, 'ticket': 'taxi'},
+        {'station': 159, 'ticket': 'taxi'}
+    ]
+    assert routes_from_158_list == sy_board.routes_from(158)
+
+
+def test_routes_from_111(sy_board):
+    routes_from_111_list = [
+        {'station': 67, 'ticket': 'underground'},
+        {'station': 79, 'ticket': 'underground'},
+        {'station': 100, 'ticket': 'bus'},
+        {'station': 110, 'ticket': 'taxi'},
+        {'station': 112, 'ticket': 'taxi'},
+        {'station': 124, 'ticket': 'taxi'},
+        {'station': 124, 'ticket': 'bus'},
+        {'station': 153, 'ticket': 'underground'},
+        {'station': 163, 'ticket': 'underground'}
+    ]
+    assert routes_from_111_list == sy_board.routes_from(111)
